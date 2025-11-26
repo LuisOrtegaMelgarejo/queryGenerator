3626af5c7a30fe984e1e0dafdeb2ba941cbed8a1
Luis Ortega <luis_ortega_m@hotmail.com>
2025-11-26 16:34:32 -0500

    test

--- main.js
+++ main.js
@@ -33,14 +33,23 @@ async function queryGenerator() {
     // Step 1: Read entire CSV to JSON array
     console.log('Step 1: Reading CSV to JSON array');
     console.log('=========================================');
-    const data = await reader.readToJSON(`../${process.argv[2] || ((new Date().getDate()) + '-' + mapperMonth[new Date().getMonth() + 1])}.csv`);
+    const filename = process.argv[2] || ((new Date().getDate()) + '-' + mapperMonth[new Date().getMonth() + 1])
+    const data = await reader.readToJSON(`../${filename}.csv`);
     console.log('Data:', data);
     console.log('\n');
-    // Step 2: Grouping by document_number (if needed)
+    
+    // Step 2: Grouping by document_number (if needed) and validating length of document_number
     console.log('Step 2: Grouping by document_number');
     console.log('=========================================');
     const groupedData = [];
     data.forEach((row) => {
+      if (
+        (row.document_type === 'DNI' && row.document_number.length !== 8) ||
+        (row.document_type === 'RUC' && row.document_number.length !== 11) ||
+        (row.document_type === 'CE' && row.document_number.length !== 9)
+      ) {
+        throw new Error(`Invalid document_number length for ${row.document_number}. It should be between 8 and 15 characters.`);
+      }
       const existing = groupedData.findIndex(r => r.document_number === row.document_number);
       if (existing === -1) {
         const merchants_id = [row.merchant_id];
@@ -51,13 +60,13 @@ async function queryGenerator() {
       }
     });
     console.log('Grouped Data:', groupedData);
+
     // Step 3: Generate SQL query using QueryGenerator
     console.log('Step 3: Generating SQL query');
     console.log('=========================================');
-    const sqlQueries = groupedData.map((row) => {
+    groupedData.map((row) => {
       const query = queryGenerator.getQuery(row);
-      const fileName = process.argv[3] || 'C2ADGCS';
-      require('fs').writeFileSync(`./output/${fileName}-${row.merchants_id.join('_')}-${row.document_type}.sql`, query + '\n');
+      require('fs').writeFileSync(`../${filename}/C2ADGCS-${row.ticket??'DEFAULT'}-${row.merchants_id.join('_')}-${row.document_type}.sql`, query + '\n');
     });
   } catch (error) {
     console.error('Error reading CSV:', error.message);
--- queryGenerator.js
+++ queryGenerator.js
@@ -11,6 +11,22 @@ class QueryGenerator {
     generateEQuery(params) {
         // Generate the 'E' query
         return `
+WITH new_hierarchy_id AS (
+    SELECT
+	    parent_node_id
+    FROM
+	    core_merchant.sch_core.t_com_merchant_management
+    WHERE
+	    document_number = '${params.document_number}'
+)
+UPDATE core_merchant.sch_core.t_com_merchant_management bh
+SET     
+    parent_node_id = nh.parent_node_id,
+    user_updated = 'tania.guizado@culqi.com',
+    updated_date = NOW()
+FROM new_hierarchy_id nh
+WHERE bh.merchant_id IN (${params.merchants_id.map(id => `'${id}'`).join(",")});
+
 WITH new_owner AS (
     SELECT
 	    com_owner_id
@@ -28,15 +44,7 @@ SET
     user_updated = 'tania.guizado@culqi.com',
     updated_date = NOW()
 FROM new_owner no
-WHERE mm.merchant_id IN (${params.merchants_id.map(id => `'${id}'`).join(",")});
-
-UPDATE core_merchant.sch_core.t_jer_business_hierarchy bh
-SET name = '${params.business_name}', updated_date = NOW()
-WHERE bh.jer_business_hierarchy_id = (
-    SELECT mm.parent_node_id
-    FROM core_merchant.sch_core.t_com_merchant_management mm
-    WHERE mm.merchant_id = '${params.merchants_id[0]}'
-);`;
+WHERE mm.merchant_id IN (${params.merchants_id.map(id => `'${id}'`).join(",")});`;
     }
 
     generateNEQuery(params) {
@@ -79,14 +87,63 @@ SET
 FROM new_owner no
 WHERE mm.merchant_id IN (${params.merchants_id.map(id => `'${id}'`).join(",")});
 
-
-UPDATE core_merchant.sch_core.t_jer_business_hierarchy bh
-SET name = '${params.business_name}', updated_date = NOW()
-WHERE bh.jer_business_hierarchy_id = (
-    SELECT mm.parent_node_id
-    FROM core_merchant.sch_core.t_com_merchant_management mm
-    WHERE mm.merchant_id = '${params.merchants_id[0]}'
-);`;
+WITH new_hierarchy_id AS (
+    INSERT INTO core_merchant.sch_core.t_jer_business_hierarchy (
+        name,
+        level_type,
+        level_type_code,
+        node_type,
+        node_type_code,
+        share_token_indicator_type,
+        share_token_indicator_type_code,
+        daily_report_indicator_type,
+        daily_report_indicator_type_code,
+        card_number_masking_indicator_type,
+        card_number_masking_indicator_type_code,
+        compression_indicator_type,
+        compression_indicator_type_code,
+        debtor_balance_attachment_indicator_type,
+        debtor_balance_attachment_indicator_type_code,
+        payment_date_indicator_type,
+        payment_date_indicator_type_code,
+        transaction_date_time_visibility_indicator_type,
+        transaction_date_time_visibility_indicator_type_code,
+        use12_hour_format_type,
+        use12_hour_format_type_code,
+        created_date
+    ) VALUES (
+        '${params.business_name}',
+        -1588,
+        2,
+        -1588,
+        2,
+        -1041,
+        1,
+        -1307,
+        'Si',
+        -1309,
+        'Si',
+        -1311,
+        'Si',
+        -1313,
+        'Si',
+        -1315,
+        'Si',
+        -1317,
+        'Si',
+        -1451,
+        'Si',
+        NOW()
+    )
+    RETURNING jer_business_hierarchy_id
+)
+UPDATE core_merchant.sch_core.t_com_merchant_management mm
+SET
+    parent_node_id = nh.jer_business_hierarchy_id,
+    user_updated = 'tania.guizado@culqi.com',
+    updated_date = NOW()
+FROM new_hierarchy_id nh
+WHERE mm.merchant_id IN (${params.merchants_id.map(id => `'${id}'`).join(",")});`;
     }
 }
 
